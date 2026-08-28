c1843a6a4e4a7efb470425bb20d01e557b27b93d
Valerii Dutov <dutov_valeriy@mail.ru>
2026-08-28 16:30:42 +0700

    add func to widget and check with test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,17 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
-result_card_number = get_mask_card_number(input("Введите номер карты: "))
-print(result_card_number)
+test_data = [
+"Maestro 1596 8378 6870 5197",
+"Счет 64686473678894779589",
+"MasterCard 7158300734726758",
+"счет 35383033474447895560",
+"Visa Classic 6831982476737658",
+"Visa Platinum8990922113665229",
+"VisaGold5999414228426353",
+"Счет 73654108430135874305"
+]
 
-result_account_number = get_mask_account(input("Введите номер счета: "))
-print(result_account_number)
+for item in test_data:
+    result_bank_data = mask_account_card(item)
+    print(result_bank_data)
+
---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from src.masks import get_mask_account, get_mask_card_number
+import re
+
+def mask_account_card(user_bank_data: str) -> str:
+    """Функция принимает информацию от пользователя о номере банковского счета или карты в виде строки и возвращает замаскированные банковские данные"""
+    result = re.findall(r"[0-9]+", user_bank_data)
+    bank_number = "".join(result)
+    if user_bank_data.lower().strip().startswith("счет"):
+        masked_number = get_mask_account(bank_number)
+        masked_bank_data = f"Счет {masked_number}"
+    else:
+        masked_number = get_mask_card_number(bank_number)
+        result = re.findall(r"[A-Z][a-z]+", user_bank_data.strip())
+        card_name = " ".join(result)
+        masked_bank_data = f"{card_name} {masked_number}"
+    return masked_bank_data
