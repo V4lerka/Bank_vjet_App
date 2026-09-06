1b2320ea018f1ebb958ff8d7d3a40c2fc377d51d
Valerii Dutov <dutov_valeriy@mail.ru>
2026-09-06 17:15:11 +0700

    after formatting with linters

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
-from src.widget import get_date, mask_account_card
 from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 test_data = [
     "Maestro 1596 8378 6870 5197",
@@ -12,10 +12,12 @@ test_data = [
     "Счет73654108430135874305",
 ]
 
-list_operations_test = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                   {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+list_operations_test = [
+    {"id": 41428829, "state": "EXECUTED", 'date': '2019-07-03T18:35:29.512364'},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 for item in test_data:
     result_bank_data = mask_account_card(item)
