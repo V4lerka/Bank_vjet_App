98eb4860276de1ed589955f16e6e144d0ebea25e
Valerii Dutov <dutov_valeriy@mail.ru>
2026-09-16 01:31:54 +0700

    reconsider all funcs in mask.py + new test with 100 % cov!

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture(params=["q1223g0003443443f", "card_number123456789677", "#66734998478998379858 ", " №90849040953274893",
                         "1O45090948O8355757", "4843 9034 3456 2311", "visa 1234098765431234"])
 def wrong_numbers(request):
@@ -9,3 +10,18 @@ def wrong_numbers(request):
 @pytest.fixture(params=["", "      "])
 def empty_number(request):
     return request.param
+
+
+@pytest.fixture(params=[1245567687687, [324647654787438], (3423, 4567, 7699, 6565)])
+def wrong_type(request):
+    return request.param
+
+
+@pytest.fixture(params=["124", "  284", " 26 ", "1"])
+def short_len(request):
+    return request.param
+
+
+@pytest.fixture
+def big_len():
+    return "576897535467458997897886"
