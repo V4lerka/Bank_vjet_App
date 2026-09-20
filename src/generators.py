1b75f7c4b93940958d80828b602514fe2f08524d
Valerii Dutov <dutov_valeriy@mail.ru>
2026-09-21 01:49:01 +0700

    improve funcs and add tests

--- src/generators.py
+++ src/generators.py
@@ -4,13 +4,27 @@ from typing import Any, Generator
 def filter_by_currency(list_transactions: list[dict[str, Any]], currency: str) -> Generator[dict[str, Any]]:
     """Функция принимает на вход список словарей, представляющих транзакции, и возвращает итератор, который поочередно
     выдает транзакции, где валюта операции соответствует заданной"""
-    for transaction in list_transactions:
-        if transaction["operationAmount"]["currency"]["code"] == currency:
-            yield transaction
+    if len(list_transactions) == 0:
+        yield {}
+    elif len(list_transactions) > 0:
+        currency_list = [transaction.get("operationAmount").get("currency").get("code") for transaction in
+                         list_transactions if transaction.get("operationAmount").get("currency") is not None]
+        if currency not in currency_list:
+            raise Exception(f"Операции с валютой {currency} не обнаружены")
+        for transaction in list_transactions:
+            if transaction.get("operationAmount").get("currency") is None:
+                continue
+            elif transaction.get("operationAmount").get("currency").get("code") == currency:
+                yield transaction
+
 
 def transaction_descriptions(list_transactions: list[dict[str, Any]]) -> Generator[str]:
     """Принимает список словарей с транзакциями и возвращает итератор, который выдает описание каждой операции
     по очереди"""
+    if len(list_transactions) == 0:
+        yield "Транзакции не найдены"
     for transaction in list_transactions:
-        yield transaction["description"]
-
+        if transaction.get("description") is not None:
+            yield transaction["description"]
+        else:
+            yield f"Описание транзакции с id {transaction.get("id", "Неизвестен")} отсутствует"
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -6,39 +6,59 @@ from tests.conftest import list_of_transactions
 def test_filter_by_currency(list_of_transactions):
     generator = filter_by_currency(list_of_transactions, "RUB")
     assert next(generator) == {
-        "id": 142264268,
+        "id": 873106923,
         "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
+        "date": "2019-03-23T01:09:46.296404",
         "operationAmount": {
-            "amount": "79114.93",
+            "amount": "43318.34",
             "currency": {
-                "name": "RUB",
+                "name": "руб.",
                 "code": "RUB"
             }
         },
         "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188"
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160"
     }
     assert next(generator) == {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-05-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "9114.93",
-                "currency": {
-                    "name": "RUB",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 29708645243227258542",
-            "to": "Счет 85651667383060284188"
-        }
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {
+            "amount": "67314.70",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657"
+    }
+
+def test_filter_by_currency_empty():
+    generator = filter_by_currency([], "RUB")
+    assert next(generator) == {}
+
+
+def test_filter_by_currency_is_absent(list_of_transactions):
+    generator = filter_by_currency(list_of_transactions, "EUR")
+    with pytest.raises(Exception) as e:
+        next(generator)
+    assert str(e.value) == "Операции с валютой EUR не обнаружены"
+
 
 def test_transaction_descriptions(list_of_transactions):
     generator = transaction_descriptions(list_of_transactions)
     assert next(generator) == "Перевод организации"
+    assert next(generator) == "Описание транзакции с id 142264268 отсутствует"
     assert next(generator) == "Перевод со счета на счет"
-    assert next(generator) == "Перевод со счета карты на счет вклада"
+    assert next(generator) == "Перевод с карты на карту"
+    assert next(generator) == "Перевод организации"
+
+
+def test_transaction_descriptions_empty():
+    generator = transaction_descriptions([])
+    assert next(generator) == "Транзакции не найдены"
+
 
